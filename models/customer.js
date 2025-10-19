09f7d0ad2700c323620e5088d01770812b771dfb
Prince <prince935059@gmail.com>
2025-10-19 14:19:10 +0530

    add new file

---
+++ models/customer.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/relationShipDemo";
+
+main().then((res) => { console.log("connected to DB") }).catch((err) =>{ console.log(err)});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
